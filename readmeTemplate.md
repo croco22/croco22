5c94ffb8d21f9c9bb54d5a5ce862b642997601d5
philipplandeck <philipp.landeck@t-online.de>
2024-10-05 10:53:30 +0200

    Exec Version 1.0

---
+++ readmeTemplate.md
@@ -0,0 +1,14 @@
+# 🌌 **Astronomy Picture of the Day** 🌌
+
+🔭 **Title:** {{ title }}  
+📅 **Date:** {{ date }}  
+📸 **Credits:** {{ authors }}  
+
+## **What's Happening in the Cosmos Today?** 📫
+
+{{ text }}
+
+
+![{{ title }}]({{ url }})
+
+🌠 **Want the HD version?** [Click here!]({{ hdurl }})
---
+++ updateReadme.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the API key from the environment variable
+api_key = os.getenv("NASA_API_KEY")
+
+# API endpoint
+api_url = "https://api.nasa.gov/planetary/apod"
+params = {"api_key": api_key}
+response = requests.get(api_url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    title = data.get("title", "Title not found")
+    date = data.get("date", "Date not found")
+    authors = data.get("copyright", "Copyright not found")
+    text = data.get("explanation", "Explanation not found")
+    url = data.get("url", "Image URL not found")
+    hdurl = data.get("hdurl", "HD Image URL not found")
+
+    with open('readmeTemplate.md', 'r') as file:
+        markdown_content = file.read()
+
+    # Replace placeholders with variable values
+    markdown_content = markdown_content.replace('{{ title }}', title)
+    markdown_content = markdown_content.replace('{{ date }}', date)
+    markdown_content = markdown_content.replace('{{ authors }}', authors)
+    markdown_content = markdown_content.replace('{{ text }}', text)
+    markdown_content = markdown_content.replace('{{ url }}', url)
+    markdown_content = markdown_content.replace('{{ hdurl }}', hdurl)
+
+    with open('README.md', 'w') as file:
+        file.write(markdown_content)
+else:
+    exit(f"Error: {response.status_code}")
